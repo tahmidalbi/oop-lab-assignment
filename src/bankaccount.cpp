0f5404bd20e1a19a1369f294e8061b4331a2d8a0
Albi <albitahmid@gmail.com>
2025-08-24 12:48:37 +0600

    test

---
+++ src/bankaccount.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class BankAccount {
+private:
+    int accountNumber;
+    string accountHolderName;
+    double balance;
+
+public:
+
+    BankAccount(int accNum, string holderName, double initialBalance) {
+
+    }
+
+    void deposit(double amount) {
+
+    }
+
+    void withdraw(double amount) {
+
+    }
+
+    void checkBalance() const {
+
+    }
+
+    void displayAccountInfo() const {
+
+    }
+};
+
+int main() {
+
+
+    return 0;
+}
